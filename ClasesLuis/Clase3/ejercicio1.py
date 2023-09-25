14d75e172ac79236a5f0b95d8497bf44c17b21a3
Jaime Nevado Farfán <djamperiio@gmail.com>
2023-09-25 18:55:02 +0200

    Clase3

---
+++ ClasesLuis/Clase3/ejercicio1.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Solicitar al usuario que ingrese su altura
+altura = float(input("Por favor, ingresa tu altura: "))
+
+# Mostrar un mensaje que incluye las variables nombre y altura
+print("Hola, mi nombre es ", nombre, " y mido ", altura, " metros.")
