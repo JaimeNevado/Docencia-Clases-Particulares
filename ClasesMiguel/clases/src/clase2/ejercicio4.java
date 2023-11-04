5a507703d20fc3179096818e380695471a3a5069
Jaime Nevado Farfán <djamperiio@gmail.com>
2023-11-04 15:15:05 +0100

    gitignore

---
+++ ClasesMiguel/clases/src/clase2/ejercicio4.java
@@ -0,0 +1,13 @@
+package clase2;
+
+public class ejercicio4 {
+	public static void main(String[] args) {
+		// Definir la variable nombre
+		int notas[] = {10, 5, 3};
+		
+		double media = (notas[0] + notas[1] + notas[2])/3;
+		
+		System.out.println("La nota media es: " + media);
+		
+	}
+}
