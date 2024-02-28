54ede7c84263609733714a5fb36605e81f7c5bf9
Jaime Nevado Farfán <djamperiio@gmail.com>
2024-02-28 18:32:53 +0100

    28Feb2024

--- ClasesPaco/Clase1/explicaciones.py
+++ ClasesPaco/Clase1/explicaciones.py
@@ -1,14 +1,19 @@
 from openai import OpenAI
 
-OPENAI_API_KEY = "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"
+archivo = open("APY_KEY", "r")
+OPENAI_API_KEY = archivo.readline()
+archivo.close()
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "Eres una maquina que genera numeros aleatorios del 2 al 5"},
-    {"role": "user", "content": "Escribe 5 numeros separados por espacio"}
+    {
+		"role": "system", 
+	    "content": "Eres una maquina que genera numeros aleatorios del 2 al 5"
+	},
+    {"role": "user", "content": "Escribe 10 numeros separados por espacio"}
   ]
 )
 
