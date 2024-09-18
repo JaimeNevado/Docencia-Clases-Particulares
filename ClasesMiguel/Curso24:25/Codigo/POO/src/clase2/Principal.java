0ae71a34f19ad613e59e22c77a9dfae879353f56
Jaime Nevado Farfán <djamperiio@gmail.com>
2024-09-18 14:49:41 +0200

    18Sep2024

---
+++ ClasesMiguel/Curso24:25/Codigo/POO/src/clase2/Principal.java
@@ -0,0 +1,14 @@
+package clase2;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Persona nueva = new Persona(10, 10, "Aikel");		
+		
+		nueva.setNombre("Maikel");
+		
+		System.out.println(nueva);
+
+	}
+
+}
